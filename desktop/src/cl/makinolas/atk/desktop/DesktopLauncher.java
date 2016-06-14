6bde8352813c2a22917639d8b4912c66b396a084
Joaquin <juako.torres@hotmail.com>
2016-06-14 16:19:06 -0400

    Proyecto Gradle

---
+++ desktop/src/cl/makinolas/atk/desktop/DesktopLauncher.java
@@ -0,0 +1,12 @@
+package cl.makinolas.atk.desktop;
+
+import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
+import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
+import cl.makinolas.atk.Main;
+
+public class DesktopLauncher {
+	public static void main (String[] arg) {
+		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
+		new LwjglApplication(new Main(), config);
+	}
+}
