a829cc0c8d373d803784a771f6832f0b746306ae
Joaquin <juako.torres@hotmail.com>
2016-06-15 22:56:54 -0400

    Enemigos

--- core/src/cl/makinolas/atk/actors/Fireball.java
+++ core/src/cl/makinolas/atk/actors/Fireball.java
@@ -1,12 +1,11 @@
 package cl.makinolas.atk.actors;
 
 import com.badlogic.gdx.Gdx;
-import com.badlogic.gdx.Input.Keys;
 import com.badlogic.gdx.graphics.Texture;
 import com.badlogic.gdx.graphics.g2d.Animation;
+import com.badlogic.gdx.graphics.g2d.Animation.PlayMode;
 import com.badlogic.gdx.graphics.g2d.Batch;
 import com.badlogic.gdx.graphics.g2d.TextureRegion;
-import com.badlogic.gdx.graphics.g2d.Animation.PlayMode;
 import com.badlogic.gdx.math.Vector2;
 import com.badlogic.gdx.physics.box2d.Body;
 import com.badlogic.gdx.physics.box2d.BodyDef;
