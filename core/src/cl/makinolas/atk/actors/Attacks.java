c8dba8c25ca41830615641ac91496399b741d2ff
CesarCaceres <cesarmanuelcaceresvalenzuela@gmail.com>
2016-06-17 19:33:23 -0300

    holi

--- core/src/cl/makinolas/atk/actors/Attacks.java
+++ core/src/cl/makinolas/atk/actors/Attacks.java
@@ -1,8 +1,11 @@
 package cl.makinolas.atk.actors;
 
 public abstract class Attacks extends AnimatedActor {
+	
   public abstract int getAttackDamage();
+  
   public abstract Monsters getSource();
+  
   public abstract void setDead();
   
   
