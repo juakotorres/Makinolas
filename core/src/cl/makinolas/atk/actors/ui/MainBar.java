1cb13d665cd193e19fe9dd5951db89ab55096d66
Sergio <elxsergio@gmail.com>
2016-06-17 15:25:14 -0400

    Fonts y barra sincronizada con Hero

--- core/src/cl/makinolas/atk/actors/ui/MainBar.java
+++ core/src/cl/makinolas/atk/actors/ui/MainBar.java
@@ -5,6 +5,7 @@ import cl.makinolas.atk.actors.Hero;
 import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.graphics.Texture;
 import com.badlogic.gdx.graphics.g2d.Batch;
+import com.badlogic.gdx.graphics.g2d.BitmapFont;
 import com.badlogic.gdx.graphics.g2d.Sprite;
 import com.badlogic.gdx.graphics.g2d.TextureRegion;
 import com.badlogic.gdx.scenes.scene2d.Actor;
@@ -14,13 +15,15 @@ public class MainBar extends Actor{
     private Hero hero;
     private HBar healthBar, magicBar;
     private Sprite base;
+    private BitmapFont font;
 
     public MainBar(Hero h){
         hero = h;
-        healthBar = new HBar(100,100,100,10,new TextureRegion(new Texture(Gdx.files.internal("Overlays/bar_green.png"))));
-        magicBar = new HBar(100,100,100,10,new TextureRegion(new Texture(Gdx.files.internal("Overlays/bar_blue.png"))));;
+        healthBar = new HBar(100,100,150,10,new TextureRegion(new Texture(Gdx.files.internal("Overlays/bar_green.png"))));
+        magicBar = new HBar(100,100,150,10,new TextureRegion(new Texture(Gdx.files.internal("Overlays/bar_blue.png"))));;
         base = new Sprite(new Texture(Gdx.files.internal("Overlays/bar_base.png")));
-        base.setRegionWidth(640);
+        base.setRegionWidth(640); //Full width
+        font = new BitmapFont(Gdx.files.internal("Fonts/normal.fnt"),Gdx.files.internal("Fonts/normal.png"),false);
         setZIndex(1000);
     }
 
@@ -28,8 +31,11 @@ public class MainBar extends Actor{
     public void draw(Batch batch, float parentAlpha) {
         float cx = getStage().getCamera().position.x - 320;
         float cy = getStage().getCamera().position.y - 240;
+        healthBar.setCurrent(hero.getHealth());
         batch.draw(base,cx,cy);
-        batch.draw(healthBar.getSprite(),cx+40,cy+24);
-        batch.draw(magicBar.getSprite(),cx+40,cy+8);
+        batch.draw(healthBar.getSprite(),cx+50,cy+26);
+        batch.draw(magicBar.getSprite(),cx+50,cy+8);
+        font.draw(batch,"HP",cx+18,cy+36);
+        font.draw(batch,"Magic",cx+4,cy+20);
     }
 }
