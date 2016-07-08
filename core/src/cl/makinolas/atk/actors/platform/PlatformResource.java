c81f26c2235601662bc8479bcadf8d6640f59e99
Sergio <elxsergio@gmail.com>
2016-07-07 21:19:48 -0400

    Platform resources in texture atlas

--- core/src/cl/makinolas/atk/actors/platform/PlatformResource.java
+++ core/src/cl/makinolas/atk/actors/platform/PlatformResource.java
@@ -1,21 +1,48 @@
 package cl.makinolas.atk.actors.platform;
 
-import cl.makinolas.atk.utils.Pair;
-import com.badlogic.gdx.Gdx;
-import com.badlogic.gdx.graphics.Texture;
+import com.badlogic.gdx.graphics.g2d.Sprite;
+import com.badlogic.gdx.graphics.g2d.TextureAtlas;
 import com.badlogic.gdx.graphics.g2d.TextureRegion;
 
 import java.util.HashMap;
 
 public class PlatformResource {
 
-    private static TextureRegion[][] platformTiles = new TextureRegion(new Texture(Gdx.files.internal("Background/platforms2.png"))).split(36,36);
-    private static HashMap<String,Pair<Integer,Integer>> textCodes = new HashMap<String, Pair<Integer,Integer>>();
+    private static TextureAtlas platAtlas = new TextureAtlas("Background/Platforms.pack");
+    private static HashMap<String,Sprite> textCodes = new HashMap<String, Sprite>();
     private static PlatformResource instance = new PlatformResource();
 
     private PlatformResource(){
-        textCodes.put("CU", Pair.fromInt(7,7));
-        textCodes.put("BC", Pair.fromInt(3,4));
+        textCodes.put("CPL",platAtlas.createSprite("ClassicPrettyLeftCorner"));
+        textCodes.put("CU",platAtlas.createSprite("ClassicUp"));
+        textCodes.put("FPR",platAtlas.createSprite("FreezePrettyRightCorner"));
+        textCodes.put("FG",platAtlas.createSprite("FreezeWithoutGrass"));
+        textCodes.put("SRL",platAtlas.createSprite("SnowRoundedLeftCorner"));
+        textCodes.put("CPR",platAtlas.createSprite("ClassicPrettyRightCorner"));
+        textCodes.put("CG",platAtlas.createSprite("ClassicWithoutGrass"));
+        textCodes.put("FRL",platAtlas.createSprite("FreezeRoundedLeftCorner"));
+        textCodes.put("FGR",platAtlas.createSprite("FreezeWithoutGrassRounded"));
+        textCodes.put("SRR",platAtlas.createSprite("SnowRoundedRightCorner"));
+        textCodes.put("YB",platAtlas.createSprite("YellowBlock"));
+        textCodes.put("CRL",platAtlas.createSprite("ClassicRoundedLeftCorner"));
+        textCodes.put("CGR",platAtlas.createSprite("ClassicWithoutGrassRounded"));
+        textCodes.put("FRR",platAtlas.createSprite("FreezeRoundedRightCorner"));
+        textCodes.put("LB",platAtlas.createSprite("LineBox"));
+        textCodes.put("SSL",platAtlas.createSprite("SnowSimpleLeftCorner"));
+        textCodes.put("CRR",platAtlas.createSprite("ClassicRoundedRightCorner"));
+        textCodes.put("CB",platAtlas.createSprite("CrossBox"));
+        textCodes.put("FSL",platAtlas.createSprite("FreezeSimpleLeftCorner"));
+        textCodes.put("SSR",platAtlas.createSprite("SnowSimpleRightCorner"));
+        textCodes.put("CSL",platAtlas.createSprite("ClassicSimpleLeftCorner"));
+        textCodes.put("FL",platAtlas.createSprite("Flame"));
+        textCodes.put("FSR",platAtlas.createSprite("FreezeSimpleRightCorner"));
+        textCodes.put("SPL",platAtlas.createSprite("SnowPrettyLeftCorner"));
+        textCodes.put("SU",platAtlas.createSprite("SnowUp"));
+        textCodes.put("CSR",platAtlas.createSprite("ClassicSimpleRightCorner"));
+        textCodes.put("FPL",platAtlas.createSprite("FreezePrettyLeftCorner"));
+        textCodes.put("FU",platAtlas.createSprite("FreezeUp"));
+        textCodes.put("SPR",platAtlas.createSprite("SnowPrettyRightCorner"));
+        textCodes.put("SGR",platAtlas.createSprite("SnowWithoutGrassRounded"));
     }
 
     public static PlatformResource getInstance() {
@@ -23,8 +50,7 @@ public class PlatformResource {
     }
 
     public TextureRegion getRegionWithCode(String code){
-        Pair<Integer,Integer> coords = textCodes.get(code);
-        return platformTiles[coords.getFirst()][coords.getSecond()];
+        return textCodes.get(code);
     }
 
 }
