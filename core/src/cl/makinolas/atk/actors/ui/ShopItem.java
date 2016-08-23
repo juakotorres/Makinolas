ef23ceb2e9dcc2c60e02161ef1841a93b38699a0
Sergio-P <elxsergio@gmail.com>
2016-08-23 12:40:40 -0300

    New overlay sprites for shop boxes

--- core/src/cl/makinolas/atk/actors/ui/ShopItem.java
+++ core/src/cl/makinolas/atk/actors/ui/ShopItem.java
@@ -2,6 +2,7 @@ package cl.makinolas.atk.actors.ui;
 
 import cl.makinolas.atk.actors.items.Item;
 import com.badlogic.gdx.Gdx;
+import com.badlogic.gdx.graphics.Texture;
 import com.badlogic.gdx.graphics.g2d.Batch;
 import com.badlogic.gdx.graphics.g2d.BitmapFont;
 import com.badlogic.gdx.graphics.g2d.TextureRegion;
@@ -10,21 +11,21 @@ import com.badlogic.gdx.scenes.scene2d.Actor;
 public class ShopItem extends Actor {
 
     private TextureRegion reg;
-    private final TextureRegion bg = Item.textures[2][3];
+    private final TextureRegion bg = new TextureRegion(new Texture("Overlays/boxgray.png"));
     private BitmapFont font = new BitmapFont(Gdx.files.internal("Fonts/normal.fnt"),Gdx.files.internal("Fonts/normal.png"),false);
     private int price;
 
     public ShopItem(TextureRegion im, int cost){
         reg = im;
         price = cost;
-        setBounds(0,0,100,48);
+        setBounds(0,0,120,48);
     }
 
     @Override
     public void draw(Batch batch, float parentAlpha) {
         float cx = getX();
         float cy = getY();
-        batch.draw(bg,cx,cy,100,48);
+        batch.draw(bg,cx,cy);
         batch.draw(reg,cx+8,cy+8);
         font.draw(batch,"$"+price,cx+42,cy+32);
     }
--- core/src/cl/makinolas/atk/screen/ShopScreen.java
+++ core/src/cl/makinolas/atk/screen/ShopScreen.java
@@ -9,10 +9,12 @@ import cl.makinolas.atk.actors.ui.ShopItem;
 import cl.makinolas.atk.utils.SaveManager;
 import com.badlogic.gdx.Game;
 import com.badlogic.gdx.Gdx;
+import com.badlogic.gdx.graphics.g2d.BitmapFont;
 import com.badlogic.gdx.graphics.g2d.TextureRegion;
 import com.badlogic.gdx.scenes.scene2d.InputEvent;
 import com.badlogic.gdx.scenes.scene2d.InputListener;
 import com.badlogic.gdx.scenes.scene2d.Stage;
+import com.badlogic.gdx.scenes.scene2d.ui.Label;
 import com.badlogic.gdx.scenes.scene2d.ui.Skin;
 import com.badlogic.gdx.scenes.scene2d.ui.TextButton;
 import com.badlogic.gdx.scenes.scene2d.utils.ClickListener;
@@ -21,16 +23,19 @@ import com.badlogic.gdx.utils.viewport.FitViewport;
 
 public class ShopScreen extends SimpleScreen{
 
-    private String[] items = {"Potion","Max Potion","PokeBall","GreatBall","UltraBall"};
-    private int[] prices = {200,1000,200,400,800};
-    private TextureRegion[] sps = {Item.textures[1][0],Item.textures[1][3], Ball.BallType.POKEBALL.texture,
-            Ball.BallType.GREATBALL.texture, Ball.BallType.ULTRABALL.texture};
+    private String[] items = {"PokeBall","GreatBall","UltraBall","Potion","Max Potion"};
+    private int[] prices = {200,400,800,200,1000};
+    private TextureRegion[] sps = {Ball.BallType.POKEBALL.texture, Ball.BallType.GREATBALL.texture,
+            Ball.BallType.ULTRABALL.texture, Item.textures[1][0],Item.textures[1][3]};
     private int itemSel = -1;
+    private BitmapFont font = new BitmapFont(Gdx.files.internal("Fonts/normal.fnt"),Gdx.files.internal("Fonts/normal.png"),false);
+    private Label currentItem, currentMoney;
 
     public ShopScreen(Game g) {
         super(g, new Stage(new FitViewport(640,480)));
         createShopItems();
-        TextButton exitButton = new TextButton("Exit Shop",  new Skin(Gdx.files.internal("Data/uiskin.json")));
+        Skin uskin = new Skin(Gdx.files.internal("Data/uiskin.json"));
+        TextButton exitButton = new TextButton("Exit Shop", uskin);
         exitButton.setPosition(20, 20);
         exitButton.addListener(new ClickListener(){
             @Override
@@ -39,7 +44,7 @@ public class ShopScreen extends SimpleScreen{
             }
         });
         stage.addActor(exitButton);
-        TextButton buyButton = new TextButton("Buy Item",  new Skin(Gdx.files.internal("Data/uiskin.json")));
+        TextButton buyButton = new TextButton("Buy Item", uskin);
         buyButton.setPosition(420, 20);
         buyButton.addListener(new ClickListener(){
             @Override
@@ -48,7 +53,7 @@ public class ShopScreen extends SimpleScreen{
             }
         });
         stage.addActor(buyButton);
-        TextButton buy10Button = new TextButton("Buy 10 Items",  new Skin(Gdx.files.internal("Data/uiskin.json")));
+        TextButton buy10Button = new TextButton("Buy 10 Items", uskin);
         buy10Button.setPosition(520, 20);
         buy10Button.addListener(new ClickListener(){
             @Override
@@ -57,6 +62,12 @@ public class ShopScreen extends SimpleScreen{
             }
         });
         stage.addActor(buy10Button);
+        currentItem = new Label("",uskin);
+        currentItem.setPosition(440,60);
+        stage.addActor(currentItem);
+        currentMoney = new Label("$"+Hero.getInstance().getInventory().getMoney(),uskin);
+        currentMoney.setPosition(280,20);
+        stage.addActor(currentMoney);
     }
 
     private void exitShop() {
@@ -66,7 +77,7 @@ public class ShopScreen extends SimpleScreen{
     private void createShopItems() {
         for(int i = 0; i < items.length; i++) {
             ShopItem item = new ShopItem(sps[i],prices[i]);
-            item.setPosition(120*(i%3)+20,320-60*(i/3));
+            item.setPosition(132*(i%3)+16,320-60*(i/3));
             final int finalI = i;
             item.addListener(new InputListener(){
                 @Override
@@ -81,6 +92,7 @@ public class ShopScreen extends SimpleScreen{
 
     public void setItemSel(int i){
         itemSel = i;
+        currentItem.setText("Selected: "+items[i]);
     }
 
     public void purchaseItem(int quantity){
@@ -88,6 +100,7 @@ public class ShopScreen extends SimpleScreen{
         if(itemSel==-1 && inventory.payMoney(prices[itemSel]*quantity)){
             inventory.addItem(ItemFinder.getInstance().itemForName(items[itemSel]),quantity);
             SaveManager.getInstance().saveState();
+            currentMoney.setText("$"+inventory.getMoney());
         }
     }
 
