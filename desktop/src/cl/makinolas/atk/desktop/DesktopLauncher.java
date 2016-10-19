3cc831edfa8491496dfa989337fa69b0bd6f527d
gsanhueza <gabriel_8032@hotmail.com>
2016-10-19 18:31:20 -0300

    Test pusheo a rama personal

--- desktop/src/cl/makinolas/atk/desktop/DesktopLauncher.java
+++ desktop/src/cl/makinolas/atk/desktop/DesktopLauncher.java
@@ -5,6 +5,9 @@ import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
 
 import cl.makinolas.atk.Main;
 
+/**
+ * Launcher used for PC
+ */
 public class DesktopLauncher {
 	public static void main (String[] arg) {
 		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
