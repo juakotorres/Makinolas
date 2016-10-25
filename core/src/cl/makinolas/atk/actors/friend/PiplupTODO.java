4805cfcac77b758a76da59e0ac3e20bd65a1a64c
Javier Espinoza <Javier Espinoza>
2016-10-25 17:46:40 -0300

    clases de pokemon TO DO

---
+++ core/src/cl/makinolas/atk/actors/friend/PiplupTODO.java
@@ -0,0 +1,11 @@
+package cl.makinolas.atk.actors.friend;
+
+public class PiplupTODO extends AbstractFriend {
+
+	@Override
+	protected void initLevel(int level) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
