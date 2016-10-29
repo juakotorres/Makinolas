86bf517098e99b40d60c00047d87e5a3ebbb08a8
paolocurotto <curotto2@hotmail.com>
2016-10-29 12:29:31 -0300

    platform test

---
+++ core/src/cl/makinolas/atk/actors/platform/WaterPlatform.java
@@ -0,0 +1,12 @@
+package cl.makinolas.atk.actors.platform;
+
+import com.badlogic.gdx.physics.box2d.World;
+
+public class WaterPlatform extends Platform {
+
+	public WaterPlatform(World myWorld, int x, int y, int widthTiles) {
+		super(myWorld, x, y, widthTiles);
+		
+	}
+
+}
