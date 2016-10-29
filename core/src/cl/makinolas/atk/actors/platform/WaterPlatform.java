6830230a01149893c6204a0b1d4d33a528999b68
paolocurotto <curotto2@hotmail.com>
2016-10-29 13:33:39 -0300

    platformW

--- core/src/cl/makinolas/atk/actors/platform/WaterPlatform.java
+++ core/src/cl/makinolas/atk/actors/platform/WaterPlatform.java
@@ -4,8 +4,8 @@ import com.badlogic.gdx.physics.box2d.World;
 
 public class WaterPlatform extends Platform {
 
-	public WaterPlatform(World myWorld, int x, int y, int widthTiles) {
-		super(myWorld, x, y, widthTiles);
+	public WaterPlatform(World myWorld, int x, int y) {
+		super(myWorld, "WT", x, y, 1, 1);
 		
 	}
 
