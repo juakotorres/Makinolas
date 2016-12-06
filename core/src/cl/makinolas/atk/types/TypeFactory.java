447865ab0971b1c6e6a4c7e2c3caf34c6014b897
martinpanza <martin.panza.p@gmail.com>
2016-12-06 17:57:17 -0300

    Uso de switch

--- core/src/cl/makinolas/atk/types/TypeFactory.java
+++ core/src/cl/makinolas/atk/types/TypeFactory.java
@@ -8,46 +8,47 @@ public class TypeFactory {
 
   public static IType getType(String str) {
     IType type = (IType) types.get(str);
-
+    
 	if (type == null) {
-	  if (str.equals("Bug")) {
-		type = new BugType();
-	  } else if (str.equals("Dark")) {
-		type = new DarkType();
-	  } else if (str.equals("Dragon")) {
-		type = new DragonType();
-	  } else if (str.equals("Electric")) {
-		type = new ElectricType();
-	  } else if (str.equals("Fairy")) {
-		type = new FairyType();
-	  } else if (str.equals("Fight")) {
-		type = new FightType();
-	  } else if (str.equals("Fire")) {
-		type = new FireType();
-	  } else if (str.equals("Flying")) {
-		type = new FlyingType();
-	  } else if (str.equals("Ghost")) {
-		type = new GhostType();
-	  } else if (str.equals("Grass")) {
-		type = new GrassType();
-	  } else if (str.equals("Ground")) {
-		type = new GroundType();
-	  } else if (str.equals("Ice")) {
-		type = new IceType();
-	  } else if (str.equals("Normal")) {
-		type = new NormalType();
-	  } else if (str.equals("Poison")) {
-		type = new PoisonType();
-	  } else if (str.equals("Psychic")) {
-		type = new PsychicType();
-	  } else if (str.equals("Rock")) {
-		type = new RockType();
-	  } else if (str.equals("Steel")) {
-		type = new SteelType();
-	  } else if (str.equals("Water")) {
-		type = new WaterType();
-	  }
-	  
+	  switch (str) {
+        case "Bug":  type = new BugType();
+                 break;
+        case "Dark":  type = new DarkType();
+                 break;
+        case "Dragon":  type = new DragonType();
+                 break;
+        case "Electric":  type = new ElectricType();
+                 break;
+        case "Fairy":  type = new FairyType();
+                 break;
+        case "Fight":  type = new FightType();
+                 break;
+        case "Fire":  type = new FireType();
+                 break;
+        case "Flying":  type = new FlyingType();
+                 break;
+        case "Ghost":  type = new GhostType();
+                 break;
+        case "Grass": type = new GrassType();
+                 break;
+        case "Ground": type = new GroundType();
+                 break;
+        case "Ice": type = new IceType();
+                 break;
+        case "Normal": type = new NormalType();
+        		 break;
+        case "Poison": type = new PoisonType();
+        		 break;
+        case "Psychic": type = new PsychicType();
+        		 break;
+        case "Rock": type = new RockType();
+                 break;
+        case "Steel": type = new SteelType();
+                 break;
+        case "Water": type = new WaterType();
+                 break;
+      }
+		
 	  types.put(str, type);
 	}
 	return type;
